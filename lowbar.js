f2627f4afcf6900d546a0f2b04a79e4afb6e754a
Gemma <gemmawvoakes@gmail.com>
2017-07-31 16:13:30 +0100

    [_.first] uses second argument to return stated number of elements.

--- lowbar.js
+++ lowbar.js
@@ -1,11 +1,13 @@
 var _ = {};
 
-_.identity = function(value) {
+_.identity = function (value) {
   return value;
 };
 
-_.first = function(arr, num) {
-  if (Array.isArray(arr) || typeof arr === 'string' && !num) return arr[0];
+_.first = function (arr, num) {
+  if (!num && Array.isArray(arr) || !num && typeof arr === 'string') return arr[0];
+  else if (Array.isArray(arr)) return arr.slice(0, num);
+  else if (typeof arr === 'string') return arr.split('').slice(0, num);
 };
 
 if (typeof module !== 'undefined') {
