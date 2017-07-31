bdfb02aa9e3a9a48d44968ed3a1ce388b86f4610
Gemma <gemmawvoakes@gmail.com>
2017-07-31 15:09:32 +0100

    [_.first] returns the first element of an array

--- lowbar.js
+++ lowbar.js
@@ -4,6 +4,10 @@ _.identity = function(value) {
   return value;
 };
 
+_.first = function(arr, num) {
+  if (Array.isArray(arr) || typeof arr === 'string' && !num) return arr[0];
+};
+
 if (typeof module !== 'undefined') {
   module.exports = _;
 }
