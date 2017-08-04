1aceb25f8780cf3f3ebc40b07f75eca4ee61f0ed
Gemma <gemmawvoakes@gmail.com>
2017-08-04 16:52:23 +0100

    [_.each] tested to demonstrate iteration over lists of elements

--- spec/lowbar.js
+++ spec/lowbar.js
@@ -56,9 +56,41 @@ describe('_', function () {
       expect(_.last(false)).to.be.undefined;
       expect(_.last({ a: 5, b: 4, c: 3, d: 2, e: 1 })).to.be.undefined;
     });
-    it ('should return the stated number of elements from the end of an array or string when a second argument is given', function () {
+    it('should return the stated number of elements from the end of an array or string when a second argument is given', function () {
       expect(_.last([5, 4, 3, 2, 1], 3)).to.eql([3, 2, 1]);
       expect(_.last('Gemma', 3)).to.eql(['m', 'm', 'a']);
+    });
+  });
+  describe('#each', function () {
+    it('is a function', function () {
+      expect(_.each).to.be.a('function');
+    });
+    it('should return the list if not passed an iteratee argument', function () {
+      expect(_.each([1, 2, 3])).to.eql([1, 2, 3]);
+      expect(_.each({ a: 1, b: 2, c: 3 })).to.eql({ a: 1, b: 2, c: 3 });
+      expect(_.each('gemma')).to.eql('gemma');
+    });
+    it('should iterate over an array of elements, yielding each in turn to the iteratee function provided', function () {
+      var count = 0;
+      _.each([10, 20, 30, 40], function () { return count += 1; });
+      expect(count).to.equal(4);
+    });
+    it('should iterate over an object of elements, yielding each in turn to the iteratee function provided', function () {
+      var count = 0;
+      _.each({a:1, b:2, c:3}, function () { return count += 1; });
+      expect(count).to.equal(3);
+    });
+    it('should iterate over a string, yielding each character in turn to the iteratee function provided', function () {
+      var count = 0;
+      _.each('gemma', function () { return count += 1; });
+      expect(count).to.equal(5);
+    });
+    it('should return the list if given as an invalid data type', function () {
+      var count = 0;
+      expect(_.each(true, function () { return count += 1; })).to.equal(true);
+      expect(_.each(12345, function () { return count += 1; })).to.equal(12345);
+      expect(_.each(null, function () { return count += 1; })).to.equal(null);
+      expect(count).to.equal(0);
     });
   });
 });
