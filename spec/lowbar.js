ebde7fd492d5625daa88bec75971bd2713ad446d
Gemma <gemmawvoakes@gmail.com>
2017-08-23 16:39:31 +0100

    [_.uniq] tested on an array and returns result as expected.

--- spec/lowbar.js
+++ spec/lowbar.js
@@ -139,4 +139,26 @@ describe('_', function () {
       expect(_.filter('Gemma', function(char) { return char === 'm'; })).to.eql(['m', 'm']);
     });
   });
+  describe('#reject', function () {
+    it('is a function', function () {
+      expect(_.reject).to.be.a('function');
+    });
+    it('should return an empty array when provided with an invalid data type', function () {
+      expect(_.reject(123, function(num) { return num < 2; })).to.eql([]);
+      expect(_.reject(false, function(bool) { return bool === true; })).to.eql([]);
+    });
+    it('should return all values in the list that do not pass the truth test (predicate)', function () {
+      expect(_.reject([1, 2, 3, 4, 5, 6], function(num) { return num % 2 === 0; })).to.eql([1, 3, 5]);
+      expect(_.reject({a:1, b:2, c:3, d:4, e:5, f:6}, function (num) { return num % 2 !== 0; })).to.eql([2, 4, 6]);
+      expect(_.reject('Northcoders', function (char) { return char === 'o'; })).to.eql(['N', 'r', 't', 'h', 'c', 'd', 'e', 'r', 's']);
+    });
+  });
+  describe('#uniq', function () {
+    it('is a function', function () {
+      expect(_.uniq).to.be.a('function');
+    });
+    it('should produce a duplicate free version of the array in which only the first instance of each value is kept', function () {
+      expect(_.uniq([1, 2, 1, 4, 1, 3])).to.eql([1, 2, 4, 3]);
+    });
+  });
 });
