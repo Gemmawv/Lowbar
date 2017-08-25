110b068eefd5989f9b94bb109390ae25f0b0a1da
Gemma <gemmawvoakes@gmail.com>
2017-08-25 18:27:27 +0100

    [_.each] code tidied up.

--- lowbar.js
+++ lowbar.js
@@ -20,15 +20,15 @@ _.each = function (list, iteratee) {
   if (!iteratee) return list;
   if (Array.isArray(list) || typeof list === 'string') {
     for (var i = 0; i < list.length; i++) {
-      iteratee(list[i]);
+      iteratee(list[i], i, list);
     }
-    return list;
+
   }
-  if (typeof list === 'object' && list !== null) {
+  else if (typeof list === 'object' && list !== null) {
     for (var key in list) {
       iteratee(list[key]);
     }
-    return list;
+    
   }
   return list;
 };
