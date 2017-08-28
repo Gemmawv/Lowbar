e7008978075354e850e6b1ed928c92889d790e7d
Gemma <gemmawvoakes@gmail.com>
2017-08-28 20:24:59 +0100

    [_.some] now returns true if any value in a string or object passes the predicate truth test. Returns false if no value in an array, string or object passes.

--- lowbar.js
+++ lowbar.js
@@ -149,12 +149,17 @@ _.every = function (list, predicate) {
 };
 
 _.some = function (list, predicate) {
-if (Array.isArray(list) || typeof list === 'string') {
-  for (let i = 0; i < list.length; i++) {
-    if (predicate(list[i])) return true;
+  if (Array.isArray(list) || typeof list === 'string') {
+    for (let i = 0; i < list.length; i++) {
+      if (predicate(list[i])) return true;
+    }
+  }
+  else if (typeof list === 'object' && list !== null) {
+    for (let key in list) {
+      if (predicate(list[key]) === true) return true;
+    }
   }
   return false;
-}
 };
 
 if (typeof module !== 'undefined') {
